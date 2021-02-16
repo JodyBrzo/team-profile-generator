c477f8b155b1a4718c3538bda7fa55c6511597b0
Jody Brzovski <jody.brzovski@gmail.com>
2021-02-16 13:52:31 -0500

    testing employee.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const inquirer = require('inquirer');
-const Employee = require('./lib/Employee.js');
 const Manager = require('./lib/Manager.js');
 const Engineer = require('./lib/Engineer.js');
 const Intern = require('./lib/Intern.js');
