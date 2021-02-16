a4d65d49e31827631ca6760955911b2a6c946ba1
Jody Brzovski <jody.brzovski@gmail.com>
2021-02-16 10:30:51 -0500

    Creating staffArray

---
+++ index.js
@@ -0,0 +1,12 @@
+const inquirer = require('inquirer');
+const Employee = require('./lib/Employee.js');
+const Manager = require('./lib/Manager.js');
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
+
+const staffArray = {
+    'manager': [], 
+    'engineer': [],
+    'intern': []
+};
+
